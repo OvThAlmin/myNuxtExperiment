3c92399d99b2e18164fa96b7e7a177195af08ead
OvThAlmin <text@example.com>
2018-10-02 23:48:52 +0900

    syakyo sample nekoneko

--- pages/about.vue
+++ pages/about.vue
@@ -1,25 +1,26 @@
 <template>
-  <section class="container">
-    <div>
-      <logo/>
-      <h1 class="title">
-        心満Project
-      </h1>
-      <h2 class="subtitle">
-        My prime Full-heart project
-      </h2>
-    </div>
-  </section>
+    <section class="container">
+        <div class="columns is-multiline">
+            <!-- v-for で breed_list からループ出力 -->
+            <div v-for="(item, i) in breed_list" v-bind:key='i' class='column is-2'>
+                <nuxt-link :to="{ path: 'dogs/'+ i }" class="button">{{ i }}</nuxt-link>
+            </div>
+        </div>
+    </section>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
+import catApi from '@/api/cat'
+import {mapState} from 'vuex';
 
 export default {
-  components: {
-    Logo
-  }
+  async fetch({store}) {
+            let json = await catApi.breeds();
+            store.commit('breed_list_update', json)
+  },
+  computed: mapState(['breed_list']),  // mapState ヘルパー
 }
+
 </script>
 
 <style>
